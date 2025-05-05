2a32af1fb89a0791d50fa91cd997e2a68f179c46
Fabiano Carvalho <fbiano15@hotmail.com>
2025-05-05 16:45:03 -0300

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,14 @@
-/* MENU LATERAL */
+/* MENU MOBILE */
 
 var menu_botao = document.getElementById("hamburguer")
 var menu_lateral = document.getElementById("menu_mobile")
 
 menu_botao.addEventListener("click", function() {
     menu_lateral.classList.toggle("ativar");
+
+    if (menu_lateral.classList.contains('ativar')) {
+        menu_botao.textContent = 'close'; // troca para "X"
+    } else {
+        menu_botao.textContent = 'menu'; // volta para hambúrguer
+    }
 })
